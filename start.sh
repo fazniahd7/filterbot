4ad1273020ff2601d13dbaaab7cc2b1b1ba152d8
fazniahd7 <108814897+fazniahd7@users.noreply.github.com>
2023-01-25 07:45:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/fazniahd7/filterbot /Faznifilterbot4GB 
+  git clone https://github.com/fazniahd7/filterbot.git /EvaMaria 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Faznifilterbot4GB
+  git clone $UPSTREAM_REPO /EvaMaria
 fi
 cd /Faznifilterbot4GB
 pip3 install -U -r requirements.txt
