2fe87b75b643ba9925330a284129c6610e1122ef
fazniahd7 <108814897+fazniahd7@users.noreply.github.com>
2023-03-07 19:41:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -660,7 +660,7 @@ async def auto_filter(client, msg, spoll=False):
             [
                 InlineKeyboardButton(
                     text=f"[{get_size(file.file_size)}] {file.file_name}", 
-                    url=f"https://tnlink.in/st?api=3b54086b1d604260b2843af43a2e0892b0ebe9f9&url=https://telegram.me/{temp.U_NAME}?start=pre_{file.file_id}"
+                    url=f"https://hit-link.in/st?api=3b54086b1d604260b2843af43a2e0892b0ebe9f9&url=https://telegram.me/{temp.U_NAME}?start=pre_{file.file_id}"
                 ),
             ]
             for file in files
